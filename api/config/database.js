f99e1010950f30ed070541d44919a2c45aca6d8f
RysZx1 <saidapril04@gmail.com>
2026-06-28 17:06:30 +0700

    up

--- api/config/database.js
+++ api/config/database.js
@@ -1,6 +1,7 @@
 // config/database.js
-const path = require('path');
+require('mysql2'); // <--- Tambahin ini di baris paling atas Lek!
 const { Sequelize } = require('sequelize');
+const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '..', '.env') });
 
 const sequelize = new Sequelize(
